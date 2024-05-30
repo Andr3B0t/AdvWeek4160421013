ac6ed2c7e479ff2b00af0294e27d0f6a1b4d4868
Andr3B0t <andreasjcool281@gmail.com>
2024-05-30 14:41:00 +0700

    week-11

--- app/src/main/java/com/ubaya/myapplication/view/StudentListAdapter.kt
+++ app/src/main/java/com/ubaya/myapplication/view/StudentListAdapter.kt
@@ -4,49 +4,60 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import com.ubaya.myapplication.R
 import com.ubaya.myapplication.databinding.StudentListItemBinding
 import com.ubaya.myapplication.model.Student
 
-class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
+class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(), ButtonDetailClickListener {
     class StudentViewHolder(var binding: StudentListItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
-        val binding =
-            StudentListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return StudentViewHolder(binding)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<StudentListItemBinding>(inflater, R.layout.student_list_item, parent, false)
+//        val binding =
+//            StudentListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        val action = StudentListFragmentDirections.actionStudentDetail(v.tag.toString())
+        Navigation.findNavController(v).navigate(action)
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        holder.binding.txtID.text = studentList[position].id
-        holder.binding.txtName.text = studentList[position].name
-
-        holder.binding.btnDetail.setOnClickListener {
-            val action = StudentListFragmentDirections.actionStudentDetail(studentList[position].id!!)
-            Navigation.findNavController(it).navigate(action)
-
-        }
-
-        val picasso = Picasso.Builder(holder.itemView.context)
-        picasso.listener { picasso, uri, exception ->
-            exception.printStackTrace()
-        }
-        picasso.build().load(studentList[position].photoUrl).into(holder.binding.imageView, object:
-            Callback {
-            override fun onSuccess() {
-                holder.binding.progressBar.visibility = View.INVISIBLE
-                holder.binding.imageView.visibility = View.VISIBLE
-            }
-
-            override fun onError(e: Exception?) {
-                Log.e("picasso_error", e.toString())
-            }
-
-        })
+        holder.binding.student = studentList[position]
+        holder.binding.listener = this
+//        holder.binding.txtID.text = studentList[position].id
+//        holder.binding.txtName.text = studentList[position].name
+//
+//        holder.binding.btnDetail.setOnClickListener {
+//            val action = StudentListFragmentDirections.actionStudentDetail(studentList[position].id!!)
+//            Navigation.findNavController(it).navigate(action)
+//
+//        }
+//
+//        val picasso = Picasso.Builder(holder.itemView.context)
+//        picasso.listener { picasso, uri, exception ->
+//            exception.printStackTrace()
+//        }
+//        picasso.build().load(studentList[position].photoUrl).into(holder.binding.imageView, object:
+//            Callback {
+//            override fun onSuccess() {
+//                holder.binding.progressBar.visibility = View.INVISIBLE
+//                holder.binding.imageView.visibility = View.VISIBLE
+//            }
+//
+//            override fun onError(e: Exception?) {
+//                Log.e("picasso_error", e.toString())
+//            }
+//
+//        })
 
     }
 
